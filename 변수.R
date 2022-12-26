08fa317110c58b9f26fa39a5e790effa9c6def6c
eunjaelim <imeunjae361@gmail.com>
2022-12-26 13:53:17 +0900

    변수

---
+++ 변수.R
@@ -0,0 +1,32 @@
+A <- 10
+A
+B <- A+20
+B
+a <- 10
+b <- 20
+c <- a+b
+c
+a <-"A" 
+a
+d<-10;e<-15;f<-20
+d
+e
+f
+avg <-10
+AVG<-20
+val.a <-15
+val.b <-20
+val.A<-19
+print(avg)
+
+%변수의 값이 변함
+v1<- 10
+v1
+v1<-"Good Morning"
+v1
+
+A <- 10
+B <- "Good"
+c <- NULL
+D <- NA
+E <- TRUE
